5ff44be7b2184052c291cb334644fa5c5a13d8da
Clayton Kruse <claytonkruse08@gmail.com>
2025-04-02 13:05:05 -0500

    implement pitch strats

--- src/org/mizzoucs3330team/assignment_03/HigherPitchStratgey.java
+++ src/org/mizzoucs3330team/assignment_03/HigherPitchStratgey.java
@@ -2,10 +2,12 @@ package org.mizzoucs3330team.assignment_03;
 
 public class HigherPitchStratgey implements PitchStrategy {
 
+	/**
+	 * Raise a note's pitch by two semitones.
+	 */
 	@Override
 	public int modifyPitch(int note) {
-		// TODO Auto-generated method stub
-		return 0;
+		return note + 2;
 	}
 
 }
--- src/org/mizzoucs3330team/assignment_03/LowerPitchStrategy.java
+++ src/org/mizzoucs3330team/assignment_03/LowerPitchStrategy.java
@@ -2,9 +2,11 @@ package org.mizzoucs3330team.assignment_03;
 
 public class LowerPitchStrategy implements PitchStrategy {
 
+	/**
+	 * Lower a note's pitch by two semitones.
+	 */
 	@Override
 	public int modifyPitch(int note) {
-		// TODO Auto-generated method stub
 		return note - 2;
 	}
 
