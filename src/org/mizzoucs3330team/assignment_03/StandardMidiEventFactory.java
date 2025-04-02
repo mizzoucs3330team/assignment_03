94305ab0325f86092eb14cf76a0fc76aa4b4d676
Clayton Kruse <claytonkruse08@gmail.com>
2025-04-02 12:51:12 -0500

    skel MidiEvent factories

---
+++ src/org/mizzoucs3330team/assignment_03/StandardMidiEventFactory.java
@@ -0,0 +1,20 @@
+package org.mizzoucs3330team.assignment_03;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+
+public class StandardMidiEventFactory implements MidiEventFactory {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
