74c6ad93b5ea4ddaea4a73c2df1dbd1e94a5889f
evan52703 <128087397+evan52703@users.noreply.github.com>
2025-04-04 14:50:13 -0500

    intitial LegatoMidiEventFactory implementation

--- src/org/mizzoucs3330team/assignment_03/LegatoMidiEventFactory.java
+++ src/org/mizzoucs3330team/assignment_03/LegatoMidiEventFactory.java
@@ -1,19 +1,35 @@
 package org.mizzoucs3330team.assignment_03;
 
 import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiEvent;
+import javax.sound.midi.*;
 
 public class LegatoMidiEventFactory implements MidiEventFactory {
 
-	@Override
-	public MidiEvent createNoteOn(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private PitchStrategy pitchStrategy;
 
-	@Override
-	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void setPitchStrategy(PitchStrategy strategy) {
+        this.pitchStrategy = strategy;
+    }
+
+    @Override
+    public MidiEvent createNoteOn(int tick, int note, int channel) throws InvalidMidiDataException {
+        int modifiedNote = (pitchStrategy != null) ? pitchStrategy.modifyPitch(note) : note;
+        ShortMessage message = new ShortMessage();
+        
+		message.setMessage(ShortMessage.NOTE_ON, channel, modifiedNote, 100);
+        
+		return new MidiEvent(message, tick);
+    }
+
+    @Override
+    public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+        int modifiedNote = (pitchStrategy != null) ? pitchStrategy.modifyPitch(note) : note;
+        ShortMessage message = new ShortMessage();
+        
+		message.setMessage(ShortMessage.NOTE_OFF, channel, modifiedNote, 100);
+        
+		//+80 ticks for transition
+        return new MidiEvent(message, tick + 80);
+    }
 }
--- src/org/mizzoucs3330team/assignment_03/StandardMidiEventFactory.java
+++ src/org/mizzoucs3330team/assignment_03/StandardMidiEventFactory.java
@@ -1,8 +1,8 @@
 package org.mizzoucs3330team.assignment_03;
 
 import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiEvent;
-import javax.sound.midi.ShortMessage;
+import javax.sound.midi.*;
+//import javax.sound.midi.MidiEvent;
 
 public class StandardMidiEventFactory implements MidiEventFactory {
 
