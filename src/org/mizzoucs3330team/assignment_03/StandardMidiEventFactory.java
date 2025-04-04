e6a21d11abf3005ed35f9aafea270141ef2aeb2b
evan52703 <128087397+evan52703@users.noreply.github.com>
2025-04-04 14:43:32 -0500

    initial StandardMidiEventFactory implementation

--- src/org/mizzoucs3330team/assignment_03/StandardMidiEventFactory.java
+++ src/org/mizzoucs3330team/assignment_03/StandardMidiEventFactory.java
@@ -2,19 +2,36 @@ package org.mizzoucs3330team.assignment_03;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
 
 public class StandardMidiEventFactory implements MidiEventFactory {
 
+	private PitchStrategy pitchStrategy;
+
+	@Override
+	public void setPitchStrategy(PitchStrategy strategy){
+		this.pitchStrategy = strategy;
+	}
+
 	@Override
 	public MidiEvent createNoteOn(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		int modifiedNote = (pitchStrategy != null) ? pitchStrategy.modifyPitch(note) : note;
+		ShortMessage message = new ShortMessage();
+
+		message.setMessage(message.NOTE_ON, channel, modifiedNote, 100);
+
+		return new MidiEvent(message, tick);
 	}
 
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
-		// TODO Auto-generated method stub
-		return null;
+		int modifiedNote = (pitchStrategy != null) ? pitchStrategy.modifyPitch(note) : note;
+		ShortMessage message = new ShortMessage();
+
+		message.setMessage(message.NOTE_OFF, channel, modifiedNote, 100);
+
+		//no change
+		return new MidiEvent(message, tick);
 	}
 
 }
