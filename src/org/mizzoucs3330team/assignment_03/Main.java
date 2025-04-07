1ba2e889edbb0b5439cec9b99bbe76902c1e4651
Clayton Kruse <claytonkruse08@gmail.com>
2025-04-06 19:42:36 -0500

    add mystery song name at end 2

--- src/org/mizzoucs3330team/assignment_03/Main.java
+++ src/org/mizzoucs3330team/assignment_03/Main.java
@@ -79,6 +79,7 @@ public class Main {
 			Thread.sleep(500);
 			sequencer.close();
 
+			System.out.println();
 			System.out.println("The song was \"bad guy\" by Billie Eilish.");
 
 		} catch (Exception e) {
